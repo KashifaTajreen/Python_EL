e6affa01c5a0f499bfc315d710e567d856d6258d
KashifaTajreen <kashtaj777@gmail.com>
2025-12-12 19:29:45 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
+from textblob import TextBlob
+
+def analyze_sentiment(comment):
+    scores = [TextBlob(c).sentiment.polarity for c in comment]
+    overall_sentiment = sum(scores) / len(scores) 
+
+
 

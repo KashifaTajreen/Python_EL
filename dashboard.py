a86438f4a384782cda370ceea25bc6cdb8870307
KashifaTajreen <kashtaj777@gmail.com>
2025-12-13 23:36:52 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ from analysis_view import run_analysis_view # Import the new analysis view
 # Define the function to transition from Dashboard to Feedback Form
 def open_feedback_form(root, session):
     root.withdraw()  # Close dashboard window
-    run_gui(session) # Open the feedback form
+    run_gui(root,session) # Open the feedback form
     root.deiconify()
    
 def run_dashboard(user_email):
